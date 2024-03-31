882aa8097250aaaab4e41bf437bd437314159ae9
Constantine Khamis <costikhamis@gmail.com>
2024-03-31 16:00:57 -0700

    Implemented active_users function (thank you, Cory). Needs some touch ups

--- webspy/src/service/user_service.rs
+++ webspy/src/service/user_service.rs
@@ -1,6 +1,7 @@
 use actix_web::web;
 use chrono::Local;
-use sea_orm::{ActiveModelTrait, ActiveValue, ColumnTrait, DbErr, EntityTrait, JoinType, QueryFilter, QueryOrder, QuerySelect, RelationDef, RelationTrait};
+use sea_orm::{ActiveModelTrait, ActiveValue, ColumnTrait, ConnectionTrait, DatabaseBackend, DbBackend, DbErr, EntityTrait, JoinType, QueryFilter, QueryOrder, QueryResult, QuerySelect, QueryTrait, RelationDef, RelationTrait, Statement};
+use sea_orm::prelude::DateTimeLocal;
 use uuid::Uuid;
 use crate::data_transfer_object::new_user::NewUser;
 use crate::data_transfer_object::new_domain::NewDomain;
@@ -16,15 +17,38 @@ pub async fn all_users(db: &web::Data<AppState>) -> Vec<user::Model> {
     user::Entity::find().order_by_desc(user::Column::FirstSeen).all(&db.conn).await.unwrap()
 }
 
-pub async fn active_users(db: &web::Data<AppState>) -> Vec<(String, i64)> {
-    println!("{:?}", request::Entity::find()
-        .select_only()
-        .column(request::Column::Ip)
-        .column(request::Column::Timestamp.max())
-        .group_by(request::Column::Ip)
-        .order_by_desc(request::Column::Timestamp)
-        .all(&db.conn).await);
-    todo!() //todo: this does not work. it gives an error saying the ip column does not exist in the request table, but it is
+pub async fn active_users(db: &web::Data<AppState>) -> Vec<(String, Option<DateTimeLocal>)> {
+    let rat: Vec<(QueryResult)> = db.conn.query_all(Statement::from_string(
+        DatabaseBackend::MySql,
+        "SELECT ip, MAX(request.timestamp) AS last_seen
+            FROM web_spy.request
+            GROUP BY ip
+            ORDER BY last_seen DESC;"
+    )).await.unwrap();
+
+    rat.iter().filter_map(|ratr| {
+        let ip:Result<String, DbErr> = ratr.try_get_by(0);// try get many by index
+        let last_seen:Result<DateTimeLocal, DbErr> = ratr.try_get_by(1);
+        let raet = ip.ok().map(|a| {(a, last_seen.ok())});
+        raet
+    }).collect()
+
+
+    // println!("{:?}", request::Entity::find()
+    //     .select_only()
+    //     .column_as(request::Column::Ip, "ip")
+    //     .expr_as_(request::Column::Timestamp.max(), "last seen")
+    //     .group_by(request::Column::Ip)
+    //     .order_by_desc(request::Column::Timestamp.max())
+    //     .build(DbBackend::MySql)
+    //     .to_string());
+    // println!("{:?}", request::Entity::find()
+    //     .select_only()
+    //     .column_as(request::Column::Ip, "ip")
+    //     .expr_as_(request::Column::Timestamp.max(), "last seen")
+    //     .group_by(request::Column::Ip)
+    //     .order_by_desc(request::Column::Timestamp.max())
+    //     .all(&db.conn).await);
 }
 
 pub async fn banned_users(db: &web::Data<AppState>) -> Vec<user::Model> {
